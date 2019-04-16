1de3db4706b0f892314d12ecd57a2084b92d67e6
bonnieho <bonnieho@rice.edu>
2019-04-15 23:38:21 -0600

    updated components per video - just not sure I did it right

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,25 @@
-var gulp          = require('gulp');
-var browserSync   = require('browser-sync').create();
-var $             = require('gulp-load-plugins')();
-var autoprefixer  = require('autoprefixer');
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
 
 var sassPaths = [
-  'node_modules/foundation-sites/scss',
-  'node_modules/motion-ui/src'
+  'bower_components/normalize.scss/sass',
+  'bower_components/foundation-sites/scss',
+  'bower_components/motion-ui/src'
 ];
 
-function sass() {
+gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
     .pipe($.sass({
       includePaths: sassPaths,
       outputStyle: 'compressed' // if css compressed **file size**
     })
       .on('error', $.sass.logError))
-    .pipe($.postcss([
-      autoprefixer({ browsers: ['last 2 versions', 'ie >= 9'] })
-    ]))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.stream());
-};
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('css'));
+});
 
-function serve() {
-  browserSync.init({
-    server: "./"
-  });
-
-  gulp.watch("scss/*.scss", sass);
-  gulp.watch("*.html").on('change', browserSync.reload);
-}
-
-gulp.task('sass', sass);
-gulp.task('serve', gulp.series('sass', serve));
-gulp.task('default', gulp.series('sass', serve));
+gulp.task('default', ['sass'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+});
