f65f6a6e2fe73f8821fbdc49e3b4959cf84eb9e1
bonnieho <bonnieho@rice.edu>
2019-04-20 23:03:02 -0500

    trying to fix issue with console message jquery not defined coming from foundation cdn links

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
 
+//var $ = {},
+ // jQuery = $ = require('../bower_components/jquery/dist/jquery');
+
+/* import jQuery from 'jquery' 
+
+window.jQuery = jQuery
+window.$ = jQuery */
+
+//import $ from 'jquery';
+//import jQuery from 'jquery'; // STEP ONE**********
+//import whatInput from 'what-input';
+//window.$ = $;
+//window.jQuery = jQuery;     // STEP TWO**********
+//import Foundation from 'foundation-sites';
+
+
+
 $(function() {
 	$('gallery a').on('click',function() {
 		var image = $(this).find('img').attr('src');
 		var imageSelected = image.split('thumbnail');
 		var fullPath = imageSelected[0] + 'fullsize' + imageSelected[1];
-		$('#galleryModal #imageContainer').attr('src', fullPath);
+		//$('#galleryModal #imageContainer').attr('src', fullPath);
 		console.log(fullPath);
 	});
 });
